ea99fa7263abb0e4e3053e14d5ae36ef3ddb5fba
sssulai@gmail.com <you@example.com>
2022-05-25 19:31:41 -0400

    added authentication service

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,18 @@
-
-
 console.log('hello');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = requre('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req,res) => {
+  res.send({
+    message: 'Your user was registered! Have fun!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
