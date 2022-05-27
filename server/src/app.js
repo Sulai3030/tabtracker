feb4eed0a61afa70e76d42e588c94026203a153f
sssulai@gmail.com <you@example.com>
2022-05-27 19:04:58 -0400

    added all commits

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,15 @@
-console.log('hello');
+console.log('hello')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan = requre('morgan')
+const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req,res) => {
+app.get('/status', (req, res) => {
   res.send({
     message: 'Your user was registered! Have fun!'
   })
